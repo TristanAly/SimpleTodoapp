336390da6d202966d277213f230df19f68bc5ad6
AlyTristan <tristan.aly@gmail.com>
2022-05-31 19:44:37 +0200

    Initial Commit

---
+++ SimpleTodoapp/SimpleTodoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodoappApp.swift
+//  SimpleTodoapp
+//
+//  Created by Tristan Aly on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
